cb715f1b4303edd0f08069dc16b7e0b946745879
lauramercadob <lAura@iMac-de-Laura.local>
2024-01-26 20:24:09 -0600

    actualiza app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario () {
-    alert("Prueba al hacer clic en el botòn")
+    alert("Prueba al hacer clic en el botón");
+    return;
 } 
+
+function generarNumeroSecreto () {
+    return Math.floor(Math.random()*10)+1;
+}
+
 asignarTextoElemento('h1','Juego del número secreto');
 asignarTextoElemento('p','Indica un número del 1 al 10');
