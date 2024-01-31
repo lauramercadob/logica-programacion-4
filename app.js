935a26287ba4c5da0ec5d30133850e42960b589c
lauramercadob <lauramercadob@gmail.com>
2024-01-30 21:40:29 -0600

    Actualiza app index

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 let numeroSecreto = generarNumeroSecreto();
 
-console.log(numeroSecreto);
-
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
 
-function intentoDeUsuario () {
-    alert("Prueba al hacer clic en el botón");
+function verificarIntento () {
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value) ;
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto)
     return;
 } 
 
 function generarNumeroSecreto () {
     return Math.floor(Math.random()*10)+1;
+    
 }
 
 asignarTextoElemento('h1','Juego del número secreto');
