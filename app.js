0af89246558fc822773b36c364ad591fe52ea268
lauramercadob <lauramercadob@gmail.com>
2024-02-08 13:47:13 -0600

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -32,14 +34,28 @@ function limpiarCaja() {
 }
 
 function generarNumeroSecreto () {
-    return Math.floor(Math.random()*10)+1;
-    
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+
+    if (listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento("p", "Ya se sortearon todos los números posibles");
+    } else {
+    //si el número generado está incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+        return generarNumeroSecreto();    
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
 }
 
 
 function condicionesIniciales() {
     asignarTextoElemento('h1','Juego del número secreto');
-    asignarTextoElemento('p','Indica un número del 1 al 10');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
